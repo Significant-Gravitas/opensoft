449cf3b003353467dacb66065b2cc13717418333
Merwane Hamadi <merwanehamadi@gmail.com>
2023-09-23 15:32:24 -0700

    [Add Tests] Ensuring ships do not overlap

--- flywheel/battleship_v3/tests/test_battleship_v3.py
+++ flywheel/battleship_v3/tests/test_battleship_v3.py
@@ -31,3 +31,33 @@ def test_positioning_ships_on_the_grid(battleship_v3, player1, game):
     assert saved_placement.start_row == 3
     assert saved_placement.start_column == 'B'
     assert saved_placement.direction == Direction.HORIZONTAL
+
+from flywheel.battleship_v3.abstract_class import ShipPlacement, ShipType, Direction
+
+
+def test_ensuring_ships_do_not_overlap(battleship_v3, player1, game):
+    # Given I already have a ship placed on specific grid squares
+    placement1 = ShipPlacement(
+        game_id=game.id,
+        ship_type=ShipType.BATTLESHIP,
+        start_row=3,
+        start_column='B',
+        direction=Direction.HORIZONTAL
+    )
+    battleship_v3.create_ship_placement(game.id, placement1)
+
+    # When I try to place another ship on the same squares
+    placement2 = ShipPlacement(
+        game_id=game.id,
+        ship_type=ShipType.CRUISER,
+        start_row=3,
+        start_column='B',
+        direction=Direction.HORIZONTAL
+    )
+
+    try:
+        battleship_v3.create_ship_placement(game.id, placement2)
+        assert False, "Expected a ValueError due to overlapping ships."
+    except ValueError:
+        # Then I should be prevented from doing so
+        pass

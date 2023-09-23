8a7831189e72aaab511bd37aea0faa12e111e2c6
Merwane Hamadi <merwanehamadi@gmail.com>
2023-09-23 15:17:17 -0700

    [Add Tests] Positioning ships on the grid

--- flywheel/battleship_v3/tests/test_battleship_v3.py
+++ flywheel/battleship_v3/tests/test_battleship_v3.py
@@ -1,3 +1,6 @@
+from flywheel.battleship_v3.abstract_class import ShipPlacement, ShipType, Direction
+
+
 def test_starting_a_new_game(battleship_v3, player1, player2):
     # Given the game is launched (by the presence of the `battleship_v2` fixture)
 
@@ -8,3 +11,23 @@ def test_starting_a_new_game(battleship_v3, player1, player2):
     game = battleship_v3.get_game(game_id)
     assert game.player1_id == player1.id
     assert game.player2_id == player2.id
+
+def test_positioning_ships_on_the_grid(battleship_v3, player1, game):
+    # Given it's my turn to position ships
+    # This is ensured by creating a new game for the player1
+
+    # When I place a ship on the 10x10 grid
+    placement = ShipPlacement(
+        game_id=game.id,
+        ship_type=ShipType.BATTLESHIP,
+        start_row=3,
+        start_column='B',
+        direction=Direction.HORIZONTAL
+    )
+    battleship_v3.create_ship_placement(game.id, placement)
+
+    # Then the ship should occupy its specified grid squares
+    saved_placement = battleship_v3.get_ship_placement(game.id, ShipType.BATTLESHIP)
+    assert saved_placement.start_row == 3
+    assert saved_placement.start_column == 'B'
+    assert saved_placement.direction == Direction.HORIZONTAL

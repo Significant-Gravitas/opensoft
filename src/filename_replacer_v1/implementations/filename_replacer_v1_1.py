aa91931e46e57d1e84e18b5f7e733baa8046d864
Merwane Hamadi <merwanehamadi@gmail.com>
2023-09-24 19:13:48 -0700

    file_renamer_v1 copied from crud_module_v3

--- src/filename_replacer_v1/implementations/filename_replacer_v1_1.py
+++ src/filename_replacer_v1/implementations/filename_replacer_v1_1.py
@@ -4,9 +4,9 @@ from pathlib import Path
 
 from fastapi import FastAPI, APIRouter, HTTPException
 from typing import List
-from src.file_renamer_v1.abstract_class import ModuleCreate, ModuleRead
+from src.filename_replacer_v1.abstract_class import ModuleCreate, ModuleRead
 
-file_renamer_v1_router = APIRouter()
+filename_replacer_v1_router = APIRouter()
 
 def get_all_modules():
     cwd = Path.cwd()
@@ -20,18 +20,18 @@ def get_all_modules():
 
 
 
-@file_renamer_v1_router.get("/v1/modules/")
+@filename_replacer_v1_router.get("/v1/modules/")
 def get_modules() -> List[ModuleRead]:
     return get_all_modules()
 
-@file_renamer_v1_router.get("/v1/modules/{module_name}/")
+@filename_replacer_v1_router.get("/v1/modules/{module_name}/")
 def get_module_by_name(module_name: str) -> ModuleRead:
     all_modules = get_all_modules()
     if module_name not in [module.name for module in all_modules]:
         raise HTTPException(status_code=404, detail="Module not found")
     return ModuleRead(name=module_name)
 
-@file_renamer_v1_router.post("/v1/modules/", response_model=ModuleRead)
+@filename_replacer_v1_router.post("/v1/modules/", response_model=ModuleRead)
 def create_module(module: ModuleCreate) -> dict:
     cwd = Path.cwd()
     folder = f"{cwd}/src/{module.name}"
@@ -42,7 +42,7 @@ def create_module(module: ModuleCreate) -> dict:
             f.write("")
     return ModuleRead(name=module.name)
 
-@file_renamer_v1_router.delete("/v1/modules/{module_name}/")
+@filename_replacer_v1_router.delete("/v1/modules/{module_name}/")
 def delete_module_by_name(module_name: str):
     cwd = Path.cwd()
     module_path = f"{cwd}/src/{module_name}"

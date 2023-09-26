e1680af22fa9165e504f215938ccdf0924f6b474
Merwane Hamadi <merwanehamadi@gmail.com>
2023-09-26 16:40:49 -0700

    Standardized test names

--- src/module_fixture/v1/tests/test_backend.py
+++ src/module_fixture/v1/tests/test_backend.py
@@ -1,5 +1,23 @@
 import pytest
 
+from src.module_fixture.v1.b1.endpoint import ModuleFixture1
+
+
+@pytest.fixture
+def module_fixture():
+    return ModuleFixture1()
+
+@pytest.mark.mock
+def test_sould_succeed_one(module_fixture):
+    assert True
+
+
+@pytest.mark.mock
+def test_sould_succeed_two(module_fixture):
+    assert True
+
+import pytest
+
 
 @pytest.mark.mock
 def test_sould_succeed_three(module_fixture):
